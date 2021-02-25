6d3c77eb2ae86e78a9957b8f1c1cd0d12893b08a
Lima, Gerardo <gerardo.lima@pmi.com>
2021-02-25 11:48:39 +0000

    feat(03): attach aws managed role AWSLambdaBasicExecutionRole to aws_iam_role.iam_for_lambda

--- 03-aws-lambda/lambda.tf
+++ 03-aws-lambda/lambda.tf
@@ -19,9 +19,18 @@ data aws_iam_policy_document "AWSLambdaTrustPolicy" {
 resource aws_iam_role "iam_for_lambda" {
   name               = "iam_for_lambda"
   path               = "/service-role/"
-  assume_role_policy = data.aws_iam_policy_document.AWSLambdaTrustPolicy.json
   tags               = var.project_tags
+  assume_role_policy = data.aws_iam_policy_document.AWSLambdaTrustPolicy.json
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
 }
+
+# Alternative way to attach roles:
+# resource aws_iam_role_policy_attachment basic {
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+#   role       = aws_iam_role.iam_for_lambda.name
+# }
 
 resource aws_lambda_function "test_lambda" {
   runtime          = "nodejs14.x"
