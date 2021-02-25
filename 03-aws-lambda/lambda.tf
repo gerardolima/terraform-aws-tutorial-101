ba1c05eefe9047cfc3c03ba2184dac313aa78da6
Lima, Gerardo <gerardo.lima@pmi.com>
2021-02-25 09:42:09 +0000

    refactor(03): iam_for_lambda.assume_role_policy from tf block

--- 03-aws-lambda/lambda.tf
+++ 03-aws-lambda/lambda.tf
@@ -1,23 +1,20 @@
 // see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
+  statement {
+    actions    = ["sts:AssumeRole"]
+    effect     = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
-  ]
+  }
 }
-EOF
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.AWSLambdaTrustPolicy.json
 }
 
 resource "aws_lambda_function" "test_lambda" {
