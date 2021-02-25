940e42fac825ff62d23373285d16647dee62b270
Lima, Gerardo <gerardo.lima@pmi.com>
2021-02-25 10:57:07 +0000

    refactor(03): move var.aws_profile to variables.tf

--- 03-aws-lambda/variables.tf
+++ 03-aws-lambda/variables.tf
@@ -1,12 +1,18 @@
 /**
   resources
+  - https://www.terraform.io/docs/cli/config/environment-variables.html#tf_var_name
   - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/resource-tagging
 */
+
+variable "aws_profile" { # << $TF_VAR_aws_profile
+  description = "A previously configured profile on AWS cli tool"
+  type = string
+}
 
 variable "project_tags" {
   description = "Tags to be applied to all resources on this project"
   type        = map(string)
-  default =   {
+    default =   {
     Terraform = true
     Project   = "03-aws-lambda"
     Foo       = "bar"
